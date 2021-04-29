e38d11c864f9968282d3cf88f442305fd591ebe6
Tanvir Ahmed <42252274+tan840@users.noreply.github.com>
2021-04-29 17:49:55 +0600

    first mobile build successfull

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float moveForce = 5;
     public float turnSpeed;
+    public float horizontalSpeed = 5;
 
 
     private Rigidbody rb;
-
+    private Touch _currentPrimaryTouch;
+    Vector2 _currentTouchPosition;
+    float _firstTouchTime;
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -19,20 +23,20 @@ public class PlayerMovement : MonoBehaviour
     {
         if (Input.touchCount > 0)
         {
-             Touch _currentPrimaryTouch = Input.GetTouch(0);
-           // _currentTouchPosition = _currentPrimaryTouch.position;
-
+            _currentPrimaryTouch = Input.GetTouch(0);
+            _currentTouchPosition = _currentPrimaryTouch.position;
+            MobileInput();
             //touch started
             if (_currentPrimaryTouch.phase == TouchPhase.Began)
             {
-
+                TouchStart();
             }
-                //TouchStart();
-
+            
             //touch going on
+
             else if (_currentPrimaryTouch.phase == TouchPhase.Moved)
                 {
-
+                    RotatePlayer();
                 }
                 //TouchOnGoing();
 
@@ -45,6 +49,7 @@ public class PlayerMovement : MonoBehaviour
 
             //Note: the other two phases are Stationary & Cancelled
         }
+#if UNITY_EDITOR
         if (Input.GetKey(KeyCode.W))
         {
             MobileInput();
@@ -55,6 +60,20 @@ public class PlayerMovement : MonoBehaviour
             rb.velocity = new Vector3(rb.velocity.x, rb.velocity.y, 0f);
             //print("W false");
         }
+
+#endif
+    }
+
+    void RotatePlayer()
+    {
+        float rotY = _currentPrimaryTouch.deltaPosition.x * -horizontalSpeed * 3 * Mathf.Deg2Rad;
+                //target.Rotate(0, rotY, 0);
+                transform.DORotate(new Vector3(0, rotY, 0), 0.15f, RotateMode.LocalAxisAdd);
+    }
+
+    void TouchStart()
+    {
+        _firstTouchTime = Time.time;
     }
 
     private void MobileInput()
