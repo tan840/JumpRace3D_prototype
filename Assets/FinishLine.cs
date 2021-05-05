cc48d45149c57871f36ce90e9ecd3b8e565a7b57
Tanvir Ahmed <42252274+tan840@users.noreply.github.com>
2021-05-05 20:43:18 +0600

    Before Adib's changes

--- Assets/FinishLine.cs
+++ Assets/FinishLine.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
+
+/// <summary>
+/// the finishline
+/// </summary>
 public class FinishLine : MonoBehaviour
 {
     GameManager gameManager;
